2eaf55e19bac2f1e5ca9822ac4fa12ffafb5b02d
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-09-16 18:22:20 -0600

    Weeeeeeee

--- 01Prove-Assignment.py
+++ 01Prove-Assignment.py
@@ -1,6 +1,14 @@
 #Taking input from the user
-color = input("Please type your favorite color: ")
-
+class Format:
+    end = '\033[0m'
+    underline = '\033[4m'
+color = input("Please type your preference color: ")
+hoilday = input("Please enter your favorite Hoilday: ")
+Vidgames = input("Please type your favorite video game that you like to play: ")
 #Output
-print("Your favorite color is " + color)
+print("Your preference color is " + Format.underline + color + Format.end)
 print (type(color))
+print("Your favorite Hoilday is " + Format.underline + hoilday + Format.end)
+print (type(hoilday))
+print("Your favorite video game that you like to play is " + Format.underline + Vidgames + Format.end)
+print (type(Vidgames))
