03f94c310d7d10894b0102f62ef6b1350af8bf8b
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-09-23 22:03:35 -0600

    madlobs

---
+++ madlib.py
@@ -0,0 +1,17 @@
+#Taking input from the user
+print("Please enter the following:")
+adjective = input("adjective: ")
+animal = input("animal: ")
+verb_1 = input("verb: ")
+exclamation = input("exclamation: ")
+verb_2 = input("verb: ")
+verb_3 = input("verb: ")
+
+#Output
+print()
+print("Your story is:")
+print()
+print("The other day, I was really in trouble. It all started when I saw a very " + adjective + " " + animal + " " + verb_1 + " down the hallway. " + 
+"\"" + exclamation.capitalize + "\"!" + " I yelled. But all I could think to do was to " + verb_2 + 
+" over and over. Miraculously,that caused it to stop, but not before it tried to " + verb_3 + " right in front of my family.")
+
---
+++ madlibshort.py
@@ -0,0 +1,12 @@
+#Taking input from the user
+print("Please enter the following:")
+girl = input("A girl's name: ")
+boy = input("A boy's name: ")
+town = input("A city or town: ")
+city = input("Another city or town: ")
+
+story = "Some time ago, in" + town + " there was a witch named " + girl + ". She was lonely, so she traveled to " + city + " and found a wizard named " + boy + "." + " They were very happy together and used their magic to rid the world of hunger. "
+#Output
+print()
+print("Your story is:")
+print(story)
