b9f61c9f44238bc37cedd15c9c44d2aad67ea80a
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-09-30 22:49:13 -0600

    Week 3

---
+++ week03/Meal-Price-Calculator.py
@@ -0,0 +1,8 @@
+#Taking input from the user
+child = float(input("What is the price of a child's meal? "))
+adult = float(input("What is the price of a adult's meal? "))
+numChild = int(input("How many children are there? "))
+numAdult = int(input("How many adults are there? "))
+sales = float(input("What is the sales tax rate? "))
+
+print(f"")
