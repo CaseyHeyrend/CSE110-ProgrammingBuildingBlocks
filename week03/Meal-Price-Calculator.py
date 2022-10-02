a7902f6e6d1e983a3b2beafa968e015db3787b13
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-10-01 18:37:06 -0600

    Lesson 3

--- week03/Meal-Price-Calculator.py
+++ week03/Meal-Price-Calculator.py
@@ -1,8 +1,26 @@
 #Taking input from the user
+from urllib import robotparser
+
+
 child = float(input("What is the price of a child's meal? "))
 adult = float(input("What is the price of a adult's meal? "))
 numChild = int(input("How many children are there? "))
 numAdult = int(input("How many adults are there? "))
-sales = float(input("What is the sales tax rate? "))
+tax = float(input("What is the sales tax rate? "))
+print()
+#calculate numbers
+childTotal = child * numChild
+adultTotal = adult * numAdult
+subTotal = adultTotal + childTotal
+sales = subTotal * tax/100
+Total = subTotal + sales
+print()
+print(f"Subtotal: ${subTotal:.2f}")
+print(f"Sales Tax: ${sales}")
+print(f"Total: ${Total}")
 
-print(f"")
+print()
+pay = float(input("What is the payment amount? "))
+change = float (pay - Total)
+limited_float = round(change, 2)
+print(f"Change: ${limited_float}")
