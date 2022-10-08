179c23e2d827500a9fec2ee475ac142a3f31c107
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-10-07 21:46:13 -0600

    Add Tip

--- week4/Meal-Price-Calculator.py
+++ week4/Meal-Price-Calculator.py
@@ -5,20 +5,22 @@ adult = float(input("What is the price of a adult's meal? "))
 numChild = int(input("How many children are there? "))
 numAdult = int(input("How many adults are there? "))
 tax = float(input("What is the sales tax rate? "))
+tip = float(input("What percentage of the tip you want to give? "))
 print()
 #calculate numbers
 childTotal = child * numChild
 adultTotal = adult * numAdult
 subTotal = adultTotal + childTotal
 sales = subTotal * tax/100
-Total = subTotal + sales
+Total = subTotal + sales 
+add_tip = (Total + (0.01 * tip) * Total)
 print()
 print(f"Subtotal: ${subTotal:.2f}")
-print(f"Sales Tax: ${sales}")
-print(f"Total: ${Total}")
+print(f"Sales Tax: ${sales:.2f}")
+print(f"Total: ${Total:.2f}")
+print(f"Adding Tip to Total: ${add_tip:.2f}")
 
 print()
 pay = float(input("What is the payment amount? "))
-change = float (pay - Total)
-limited_float = round(change, 2)
-print(f"Change: ${limited_float}")
+change = float (pay - add_tip)
+print(f"Change: ${change:.2f}")
