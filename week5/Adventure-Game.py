5832d17b9b6f1b5073635ca65871d63911880d34
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-10-14 22:21:24 -0600

    Yay Code

--- week5/Adventure-Game.py
+++ week5/Adventure-Game.py
@@ -1,35 +1,35 @@
 print("Welcome to the Adventure Game!")
-print("You are walking through a dark forest and find two items: a MATCH and a FLASHLIGHT. Which one do you want to pick up?")
+print("You are walking through a dark forest. You already had a flashlight. So no worries. But you come to a fork in the forest. Which way do you go, RIGHT or LEFT?")
 
 
-c1 = input()
+direction = input()
+#Right
+if (direction.upper()=="RIGHT"):
+    print("\nYou decide to go right. So you start to walk for a bit and come across a calm river. But something is running behind the area that was behind you. Do you CROSS the river or STAY and find out what is coming?")
+    direction = input()
+    if (direction.upper()=="CROSS"):
+        print("\nLucky")
+    elif (direction.upper()=="STAY"):
+            print("\nUnlucky")
+    else:
+        print("Please pick one. CROSS or STAY?")
+    direction = input() 
 
-if (c1.upper()=="MATCH"):
-    print("\nYou pick up the match and strike it, and for an instant, the forest around you is illuminated. You see a large grizzly bear, and then the match burns out. Do you want to RUN, or HIDE behind a tree? ")
 
-elif (c1.upper()=="FLASHLIGHT"):
-    print("\nYou pick up the flashlight and turn it on. You see the pathway lit up in front of you, but you thought you also heard something off to the side. Do you want to FOLLOW the path or LOOK in the trees for the thing that made the noise?")
+#Left
+elif (direction.upper()=="LEFT"):
+    print("\nYou decide to go left. So you walk for a bit and come across an abandoned cabin. You can see that the cabin is old but still usable. Do you EXPLORE the cabin or KEEP walking on the path?")
+    
+
+
+direction = input()
+if (direction.upper()=="EXPLORE"):
+    print("\n Lucky")
+elif (direction.upper()=="KEEP"):
+    print("\nYou decide to keep going on the path and find the abandoned fresh campsite. Then, you decide to call the rangers. But instead, rangers come and call the police.  ")
 else:
-    print("Please pick one. MATCH or FLASHLIGHT?")
-    c1 = input()
-
-c1_1 = input()
-if (c1_1.upper()=="RUN"):
-    print("\n You run and the large grizzly bear hears you are catch you. You have make your doom. You have died. This is the end")
-elif (c1_1.upper()=="HIDE"):
-    print("\n Lucky you hide in time and the large grizzly bear doesn't notice you and the bear leaves the another direction. Do you ")
-else:
-    print("Please pick one. RUN or HIDE?")
-    c1_1 = input()
-
-c1_2 = input()
-if (c1_2.upper()=="FOLLOW"):
-    print("\n")
-elif (c1_2.upper()=="LOOK"):
-    print("\n")
-else:
-    print("Please pick one. FOLLOW or LOOK?")
-    c1_2 = input()
+    print("Please pick one. EXPLORE or KEEP?")
+    direction = input()
 
 
 
