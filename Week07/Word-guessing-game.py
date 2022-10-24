16a96d3231db40630553ffbe4b8e4e8adeadff7d
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-10-23 23:23:51 -0600

    hmmm

--- Week07/Word-guessing-game.py
+++ Week07/Word-guessing-game.py
@@ -4,38 +4,35 @@ import random
 
 print("Welocme to the word guessing game!")
 
-name = input("What is your name? ")
-print("Good luck!", name)
-
 words = ['autumn', 'halloween', 'pumpkin', 'magic', 'fall', 'witch', 
         'football', 'turkey', 'october', 'spirit', 'snow', 'winter']#12 words
 
 #Function will choose one random
 #word from this list of words
-word = random.choice(words)
+random_word = random.choice(words)
+print("our random word", random_word)
 
 print("Guess the characters")
 guesses = ''
 # any number of turns can be used here
 turns = 25
 
-while turns > 0:
-    #counts the number of times a user fails
+while turns > 0:#counts the number of times a user fails 
     failed = 0
 
-    for char in word:
-            if char in guesses:
-                print(char, end=" ")
+    for character in random_word:
+        if character in guesses:
+            print(character, end= " ")
 
-            else:
-                print("_")
-                print(char, end=" ")
-                failed += 1
+        else:
+            print("_")
+            print(character, end= " ")
+            failed += 1
             
     if failed == 0:
         print()
         print("You have won!")
-        print("The word is: ",word)
+        print("The word is: ",random_word)
         break
 
     print()
@@ -43,7 +40,7 @@ while turns > 0:
     guess = input("guess a letter: ")
     guesses += guess
 
-    if guess not in word:
+    if guess not in random_word:
         turns -= 1
 
         print("Wrong answer")
--- Week07/WordGuessingGame.py
+++ Week07/WordGuessingGame.py
@@ -2,3 +2,11 @@
 import random
 
 print("Welocme to the word guessing game!")
+
+secret_word = "Halloween"
+user_guess = input("Please guess the secret word: ")
+
+while user_guess != secret_word:
+    user_guess = input("Please guess the secret word again: ")
+else:
+    print("Correct")
