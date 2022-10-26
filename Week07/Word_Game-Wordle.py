9f6d9ae1c20813b49c3b7bebb58745107762a9d0
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-10-26 01:08:54 -0600

    close to wordle

--- Week07/Word_Game-Wordle.py
+++ Week07/Word_Game-Wordle.py
@@ -1,6 +1,25 @@
-import secrets
-
-
-secret = "magic"
+secret_word = "magic"
 
 print("Welocme to the word guessing game!")
+
+secret_word = "magic"
+
+guesses = ''
+turns = 6
+while turns > 0:#counts the number of times a user fails 
+    guess = str(input("Guess the word: "))
+    if guess == secret_word:
+        print("You guessed the word correctly!")
+        break
+    else:
+        turns = turns - 1
+        print(f"You have {turns} attempt(s),\n ")
+        for char, word in zip(secret_word, guess):
+            if word in  secret_word and word in char:
+                print(word + "_")
+            elif word in secret_word:
+                print(word + "_")
+            else:
+                print("_")
+        if turns == 0:
+            print ("Game over!")
