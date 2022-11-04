9581e9affb50f14e6b3bac2693bd8f8353221a23
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-11-03 20:37:27 -0600

    done

--- Week08/Iterating-Through-Strings_Team-Activity_Stretch.py
+++ Week08/Iterating-Through-Strings_Team-Activity_Stretch.py
@@ -1,18 +1,15 @@
 quote = "In coming days, it will not be possible to survive spiritually without the guiding, directing, comforting, and constant influence of the Holy Ghost."
 
-run_again = "yes"
-while run_again == "yes":
+play_again = "yes"
+while play_again == "yes":
     user_number = int(input("Please enter a number: "))
-
     for n, letter in enumerate(quote):
         if n % user_number == 0:
             print(letter.upper(), end="")
         else:
             print(letter.lower(), end="")
-    
     print()
-
-    run_again = input("Would you like to enter another number? ")
+    play_again = input("\nWould you like to enter another number? ")
 print()
 
 print("Goodbye!")
