38ebdedaf1d98a7d4e86e613f9c7bd3c59c10a0b
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-11-05 19:53:17 -0600

    Turns was missing

--- Week08/Word_Game-Wordle.py
+++ Week08/Word_Game-Wordle.py
@@ -3,6 +3,7 @@ hidden = "frost"
 guess = " "
 
 count = 0
+turns = 8
 print("Welcome to the Word Game!")
 print("Hint: _ _ _ _ _ ")
 while guess != hidden:
@@ -14,5 +15,10 @@ while guess != hidden:
         elif letter.lower() in guess:
             hint += letter.lower()
         else:
+            turns = turns - 1
+            print(f"You have {turns} attempt(s),\n ")
             hint += "_ "
+            if turns == 0:
+                print("Game Over!")
     print(f"Your hint is: {hint}")
+    print(f"You took {turns} attempt(s).\n")
