3bace0a96e8f489aac29ba3adb958f9ba94554ce
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-11-10 20:03:06 -0700

    No

--- Week09/Lists-of-Numbers_Team-Activity-Group.py
+++ Week09/Lists-of-Numbers_Team-Activity-Group.py
@@ -1,40 +1,41 @@
-print("Enter a list of numbers, type 0 when finished.")
+
 numbers = []
 number = -1
 
+print("Enter a list of numbers, type 0 when finished.")
+
 while number != 0:
     number = int(input("Enter number: "))
-
-    if number != 0:
-        numbers.append(number)
+    numbers.append(number)
 #Sum Part of Core 1
 sum = 0
 for number in numbers:
     sum += number
 print(f"The sum is: {sum}")
 #average Part of Core 2
-count = len(numbers)
-average = sum / count
+counting = len(numbers)
+average = sum / (counting - 1)
+
 print(f"The average is: {average}")
 #largest number(Max) Part of Core 3
-max_num_so_far = -1
+max_num = -1
 for number in numbers:
-    if number > max_num_so_far:
-        max_num_so_far = number
-print(f"The largest number is: {max_num_so_far}")
+    if number > max_num:
+        max_num = number
+print(f"The largest number is: {max_num}")
 
 #Stretch Challenge 1
-min_num_so_far = 999999999999
+min_num = 999999999999
 
 for number in numbers:
-    if number > 0 and number < min_num_so_far:
+    if number > 0 and number < min_num:
         min_num_so_far = number
-print(f"The smallest positive number is: {min_num_so_far}")
+print(f"The smallest positive number is: {min_num}")
 
 #Stretch Challenge 2
-sorted_list = sorted(numbers)
+num_list = sorted(numbers)
 print(f"The sorted list is: ")
-for number in sorted_list:
+for number in num_list:
     print(number)
 
 
