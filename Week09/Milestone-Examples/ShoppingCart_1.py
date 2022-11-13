f72aa804c9aa36589b96913b6500c208162b3c34
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-11-12 20:14:42 -0700

    It works aleast

--- Week09/Milestone-Examples/ShoppingCart_1.py
+++ Week09/Milestone-Examples/ShoppingCart_1.py
@@ -3,6 +3,7 @@ cart = []
 prices = []
 total = []
 total_price = []
+item = ''
 while True:
     print()
     print ("Please type in one of these")
@@ -16,25 +17,24 @@ while True:
     if select == 1:
         while item != "ok":
             item = input(" What would you like to add?  ")
-            price = float(input(" type in the price "))
-            prices.append(price)
+            #price = float(input(" type in the price "))
+            #prices.append(price)
              
             ok = input ("type in ok when you're done.")
             if item != "ok":
                 cart.append(item)
                 print(f"'{item}' has been added to your cart.")
-                print(f" The price is ${price}")
+                #print(f" The price is ${price}")
                 break
     if select == 2:
         print("This is what is in your shopping cart")
         for item in cart:
-            print(item,price)
+            print(item)#price)
             ok = input ("press ok when you're done")
             if item != "ok":
                 break
              
-     
-                     
+                  
     if select == 3:
         takeout = input(" Type in what you would like to remove?  ")
         cart.remove(takeout)
--- Week09/Shopping_Cart.py
+++ Week09/Shopping_Cart.py
@@ -2,10 +2,10 @@
 print("Welcome to the Shopping Cart Program!")
 
 cart = []
-i_prices = []
+prices = []
 total = []
 total_price = []
-item = ""
+items = ''
 while True:
     print()
     print("Please select one of the following: ")
@@ -18,20 +18,26 @@ while True:
     option = int(input("Please enter an action: "))
     if option == 1:
         item = input("What item would you like to add? ")
-        price = float(input(f"What is the price of {item}? "))
-        print(f"'{item}' has been added to the cart.")
+        y = input("Press y to continue: ")# To confirm
+        if item != "y":
+            cart.append(item)
+            print(f"'{item}' has been added to the cart.")
+
     if option == 2:
         print("The contents of the shopping cart are: ")
         for item in cart:
-            print(item,price) 
+            print(item)
+            y = input("Press y to continue: ")# To confirm
+            if item != "y":
+                break
     if option == 3:
-        remove = int(input("Which item would you like to remove? "))
-    if option == 4:
-        for price in total_price:
-            sum += price
-            print(sum(total_price))
+        withdraw = int(input("Which item would you like to remove? "))
+        cart.remove(withdraw)
+
+    #if option == 4:
+
     if option == 5:
         print("Thank you, Goodbye!")
-        print("Enter esc to leave for now!")
+        break
 
         
