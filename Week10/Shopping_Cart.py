06754e6ea8a601b92fc0cf2377789866cc4480a9
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-11-19 20:43:41 -0700

    no index can't remove and no total.

--- Week10/Shopping_Cart.py
+++ Week10/Shopping_Cart.py
@@ -4,8 +4,6 @@ print("Welcome to the Shopping Cart Program!")
 cart = []
 prices = []
 total = []
-total_price = []
-items = ''
 while cart != "5":
     print("Please select one of the following: ")
     print("1. Add item")
@@ -18,30 +16,27 @@ while cart != "5":
     if option == 1:
         item = input("What item would you like to add? ")
         price = float(input(f"What is the price of {item}? "))
-        prices.append(price)
         y = input("Press y to continue: ")# To confirm
         if item != "y":
             cart.append(item)
+            prices.append(price)
             print(f"'{item}' has been added to the cart.")
-            print(f" The price is ${price:.2f}")
 
     if option == 2:
-        print("The contents of the shopping cart are: ")
-        for i in range(len()):
-            print(f"{item[i]},${price[i]}")
-        #for item in cart:
-            #print(item, price)
-            #y = input("Press y to continue: ")# To confirm
-            #if item != "y":
-                #break
+        print("\nThe contents of the shopping cart are: ")
+        for i in range(len(cart)):
+            print(f"{cart[i]} - {prices[i]}")
+            y = input("Press y to continue: ")# To confirm
+            if item != "y":
+                break
     if option == 3:
         withdraw = int(input("Which item would you like to remove? "))
         cart.remove(withdraw)
 
     if option == 4:
-        for price in total_price:
-            sum+= price
-            print(sum(total_price))
+        for i in range(len(prices)):
+            total += prices[i]
+            print(f"Total: ${total:.2f}")
             y = input("Press y to continue: ")# To confirm
             if item != "y":
                 break
