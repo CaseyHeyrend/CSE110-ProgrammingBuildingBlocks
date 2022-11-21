6f45259cd0f885ede4545207f44b7b817731fec4
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-11-21 00:21:52 -0700

    Required the whole link

--- Week11/11Checkpoint.py
+++ Week11/11Checkpoint.py
@@ -1,4 +1,4 @@
-with open("books.txt") as book_file:
+with open("/Users/xxstormynightxx/Documents/GitHub/CSE110-ProgrammingBuildingBlocks/Week11/books.txt") as book_file:
     for line in book_file:
         book = line.strip()
         print(book)
