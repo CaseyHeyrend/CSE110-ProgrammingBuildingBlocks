479536aabfcbdc50016284c5a3ceabf110ae1089
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-11-25 15:34:58 -0700

    Today

--- Week11/Human-Resources-System_Team-Activity_StretchChallenges.py
+++ Week11/Human-Resources-System_Team-Activity_StretchChallenges.py
@@ -1,7 +1,19 @@
 with open("/Users/xxstormynightxx/Documents/GitHub/CSE110-ProgrammingBuildingBlocks/Week11/hr_system.txt") as f:
     for line in f:
-        parts = line.split(" ")
-        name = parts[0]
-        title = parts[2]
+        
+        clean_line = line.strip()#Stretch Challenge
+        
+        parts = clean_line.split(" ")#Core minus the clean part
 
-        print(f"Name: {name}, Title: {title}")
+        name = parts[0]#Core
+        id_number = parts[1]#Stretch Challenge
+        title = parts[2]#Core
+        salary = float(parts[3])#Stretch Challenge
+
+        #Stretch Challenge
+        pay_amount = salary / 24 
+        if title.lower() == "engineer":
+            pay_amount += 1000
+
+        #print(f"Name: {name}, Title: {title}")#Core  
+        print(f"{name} (ID: {id_number}), {title} - ${pay_amount:.2f}")#Stretch Challenge
