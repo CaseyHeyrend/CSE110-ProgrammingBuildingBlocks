be4b424b60a420f139cb69f2cb0a4dc6106736d6
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-11-27 22:12:20 -0700

    The Countries

--- Week11/Milestone_life-expectancy.py
+++ Week11/Milestone_life-expectancy.py
@@ -3,7 +3,7 @@ high_vaule = -1
 min_vaule = 10000000
 finest_life = -1
 min_life = 10000000
-greatest_Country = ""
+best_Country = ""
 min_Country = ""
 max_entity = ""
 min_entity = ""
@@ -12,6 +12,7 @@ min_year = ""
 counter = 0
 total_age = 0
 year_interst = input("Enter the year of interest: ")
+
 
 
 
@@ -24,7 +25,7 @@ with open ("/Users/xxstormynightxx/Documents/GitHub/CSE110-ProgrammingBuildingBl
         year = parts [2]
         expectancy = float(parts[3])
 
-        if expectancy >high_vaule:
+        if expectancy > high_vaule:
             high_vaule = expectancy
             max_entity = entity
             max_year = year
@@ -38,6 +39,13 @@ with open ("/Users/xxstormynightxx/Documents/GitHub/CSE110-ProgrammingBuildingBl
             average = total_age / counter
             if expectancy > high_vaule:
                 high_vaule = expectancy
+print(f"The overall max life expectancy is: {high_vaule:.2f} from {best_Country} in {max_year}")
+print(f"The overall min life expectancy is: {min_vaule:.2f} from {min_Country} in {min_year}")
+
+print(f"\nFor the year {year_interst}:")
+print(f"The average life expectancy across all countries was {average:.2f}")
+print(f"The max life expectancy was in {best_Country} with {high_vaule:.2f}")
+print(f"The min life expectancy was in {min_Country} with {min_vaule:.2f}")
 
 
 
