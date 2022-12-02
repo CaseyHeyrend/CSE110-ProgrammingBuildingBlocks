534d5b7a576d084986c674677b421e5d30d66d56
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-12-01 20:04:59 -0700

    Team Group

--- Week12/Finding-Items-in-a-File_Team-Activity.py
+++ Week12/Finding-Items-in-a-File_Team-Activity.py
@@ -1,3 +1,4 @@
+'''
 max_chapters = -1
 book_w_max = ""
 
@@ -11,3 +12,22 @@ with open("Week12/books_and_chapters.txt") as books_file:
         scripture = parts[2].strip()
 
 print(f"Scripture: {scripture}, Book: {book}, Chapters: {chapters}")
+'''
+highest_chapter = -1
+highest_books = ''
+with open("Week12/books_and_chapters.txt") as books:
+    for line in books:
+        parts = line.strip().split(":")
+
+        book = parts[0]
+        chapters = int(parts[1])
+        scriptures = parts[2]
+
+        print(f"Scripture: {scriptures}, Book: {book}, Chapters: {chapters}")
+
+        if chapters > highest_chapter:
+            highest_chapter = chapters
+            highest_books = book
+
+        print(f"The book with the most chapter in the is: {highest_books}")
+        print(f"It has {highest_chapter} chapters.")
--- Week12/Finding-Items-in-a-File_Team-Activity_Stretch.py
+++ Week12/Finding-Items-in-a-File_Team-Activity_Stretch.py
@@ -1,3 +1,4 @@
+'''
 chosen_volume = input("Which  volume of scripture would you like to learn about? ")
 max_chapters = -1
 book_w_max = ""
@@ -19,3 +20,25 @@ with open("Week12/books_and_chapters.txt") as books_file:
     
 print(f"The book with the most chapter in the {chosen_volume} is: {book_w_max}")
 print(f"It has {max_chapters} chapters.")
+'''
+ask_user = input("Which  volume of scripture would you like to learn about? ")
+
+highest_chapter = -1
+highest_books = ''
+with open("Week12/books_and_chapters.txt") as books:
+    for line in books:
+        parts = line.strip().split(":")
+
+        book = parts[0]
+        chapters = int(parts[1])
+        scriptures = parts[2]
+
+        if scriptures.lower() == ask_user.lower():
+            print(f"Scripture: {scriptures}, Book: {book}, Chapters: {chapters}") 
+
+        if chapters > highest_chapter:
+            highest_chapter = chapters
+            highest_books = book
+
+        print(f"The book with the most chapter in the {ask_user} is: {highest_books}")
+        print(f"It has {highest_chapter} chapters.")
